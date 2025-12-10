5bf1d4685e272e2f5f32e41827b74eab59012474
Vinicius Vasconcelos <Vinicius.Vasconcelos+1701@2k.com>
2025-12-10 10:41:21 -0300

    feat: update lazy and configs

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,12 +4,13 @@ return {
   { "catppuccin/nvim" },
   { "shaunsingh/nord.nvim" },
   { "rebelot/kanagawa.nvim" },
-  { "navarasu/onedark.nvim", opts = { style = "darker" } },
+  -- { "navarasu/onedark.nvim", opts = { style = "darker" } },
+  { "navarasu/onedark.nvim", opts = {} },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin",
     },
   },
 }
--- nvim/lua/plugins/golang.lua
+++ nvim/lua/plugins/golang.lua
@@ -147,19 +147,35 @@ return {
   },
   {
     "nvim-neotest/neotest",
-    optional = true,
     dependencies = {
-      "fredrikaverpil/neotest-golang",
-    },
-    opts = {
-      adapters = {
-        ["neotest-golang"] = {
-          -- Here we can set options for neotest-golang, e.g.
-          -- go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
-          dap_go_enabled = true, -- requires leoluz/nvim-dap-go
-        },
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      {
+        "nvim-treesitter/nvim-treesitter", -- Optional, but recommended
+        branch = "main", -- NOTE; not the master branch!
+        build = function()
+          vim.cmd(":TSUpdate go")
+        end,
+      },
+      {
+        "fredrikaverpil/neotest-golang",
+        version = "*", -- Optional, but recommended; track releases
+        build = function()
+          vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+        end,
       },
     },
+    config = function()
+      local config = {
+        runner = "gotestsum", -- Optional, but recommended
+      }
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang")(config),
+        },
+      })
+    end,
   },
 
   -- Filetype icons
